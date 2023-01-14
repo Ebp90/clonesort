634bc1ca655891430cbcda36d537efcce630fc3b
Ebp90 <40249395+Ebp90@users.noreply.github.com>
2023-01-14 11:05:17 -0500

    First

---
+++ src/js/data/2017-05-01.js
@@ -0,0 +1,165 @@
+dataSetVersion = "2022-01-13";
+dataSet[dataSetVersion] = {};
+
+dataSet[dataSetVersion].options = [
+
+];
+
+dataSet[dataSetVersion].characterData = [
+{
+    name: "99",
+    img: "99.jpg",
+  },
+{
+    name: "Appo",
+    img: "Appo.jpg",
+  },
+{
+    name: "Bly",
+    img: "Bly.jpg",
+  },
+{
+    name: "Boil",
+    img: "Boil.jpg",
+  },
+{
+    name: "Boss",
+    img: "Boss.jpg",
+  },
+{
+    name: "Colt",
+    img: "Colt.jpg",
+  },
+{
+    name: "Crosshair",
+    img: "Crosshair.jpg",
+  },
+{
+    name: "Cut",
+    img: "Cut.jpg",
+  },
+{
+    name: "Denal",
+    img: "Denal.jpg",
+  },
+{
+    name: "Dogma",
+    img: "Dogma.jpg",
+  },
+{
+    name: "Doom",
+    img: "Doom.jpg",
+  },
+{
+    name: "Echo",
+    img: "Echo.jpg",
+  },
+{
+    name: "Fives",
+    img: "Fives.jpg",
+  },
+{
+    name: "Fixer",
+    img: "Fixer.jpg",
+  },
+{
+    name: "Fox",
+    img: "Fox.jpg",
+  },
+{
+    name: "Gree",
+    img: "Gree.jpg",
+  },
+{
+    name: "Gregor",
+    img: "Gregor.jpg",
+  },
+{
+    name: "Hardcase",
+    img: "Hardcase.jpg",
+  },
+{
+    name: "Hevy",
+    img: "Hevy.jpg",
+  },
+{
+    name: "Howzer",
+    img: "Howzer.jpg",
+  },
+{
+    name: "Hunter",
+    img: "Hunter.jpg",
+  },
+{
+    name: "Jesse",
+    img: "Jesse.jpg",
+  },
+{
+    name: "Keeli",
+    img: "Keeli.jpg",
+  },
+{
+    name: "Kix",
+    img: "Kix.jpg",
+  },
+{
+    name: "Ponds",
+    img: "Ponds.jpg",
+  },
+{
+    name: "Rex",
+    img: "Rex.jpg",
+  },
+{
+    name: "Ridge",
+    img: "Ridge.jpg",
+  },
+{
+    name: "Scorch",
+    img: "Scorch.jpg",
+  },
+{
+    name: "Sev",
+    img: "Sev.jpg",
+  },
+{
+    name: "Tech",
+    img: "Tech.jpg",
+  },
+{
+    name: "Thire",
+    img: "Thire.jpg",
+  },
+{
+    name: "Thorn",
+    img: "Thorn.jpg",
+  },
+{
+    name: "Tup",
+    img: "Tup.jpg",
+  },
+{
+    name: "Vaughn",
+    img: "Vaughn.jpg",
+  },
+{
+    name: "Warthog",
+    img: "Warthog.jpg",
+  },
+{
+    name: "Waxer",
+    img: "Waxer.jpg",
+  },
+{
+    name: "Wilco",
+    img: "Wilco.jpg",
+  },
+{
+    name: "Wolffe",
+    img: "Wolffe.jpg",
+  },
+{
+    name: "Wrecker",
+    img: "Wrecker.jpg",
+  },
+];
